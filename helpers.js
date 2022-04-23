8bd4e458f1df9de67dba8cebc8cd0e377e3b2b5b
Kevin Mathew <96816115+kevinmathew47@users.noreply.github.com>
2022-04-23 19:17:13 +0530

    MIchael..V1

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Ajuser*:  ```" + msg + "```"
+    return "✅ *Michael*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Ajuser*:  ```" + msg + "```"
+    return "🛑 *Michael*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Ajuser*:  ```" + msg + "```"
+    return "⏺️ *Michael*:  ```" + msg + "```"
 }
 
 
